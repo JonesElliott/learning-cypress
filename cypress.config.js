bff17ad6ceaeca086ab127efb838a5708ebdcc73
Elliott Jones <jones.elliott@outlook.com>
2024-02-27 21:30:22 -0600

    Setup for cypress cucumber preprocessor

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   numTestsKeptInMemory: 10, // Save tests in memory for debugging
   e2e: {
+    specPattern: ['**/*.feature', '**/*.cy.js'],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
